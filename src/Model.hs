3796ee5f5b7d619320f3b1f92088f6036429bdc5
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-08-19 12:21:29 +0200

    Update foundation, model and settings

--- src/Model.hs
+++ src/Model.hs
@@ -7,17 +7,22 @@
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE UndecidableInstances #-}
+--{-# LANGUAGE DerivingStrategies #-}
+--{-# LANGUAGE StandaloneDeriving #-}
+--{-# LANGUAGE UndecidableInstances #-}
 module Model where
 
 import ClassyPrelude.Yesod
 import Database.Persist.Quasi
+import Database.Persist.MongoDB hiding (master)
+import Language.Haskell.TH.Syntax
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
 -- at:
 -- http://www.yesodweb.com/book/persistent/
-share [mkPersist sqlSettings, mkMigrate "migrateAll"]
+-- share [mkPersist sqlSettings, mkMigrate "migrateAll"]
+--    $(persistFileWith lowerCaseSettings "config/models.persistentmodels")
+let mongoSettings = (mkPersistSettings (ConT ''MongoContext))
+ in share [mkPersist mongoSettings mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models.persistentmodels")
