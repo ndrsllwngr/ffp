8e27e588691d42da9b6e19818276616570f42679
griase94 <andy.griesbeck@gmail.com>
2020-08-30 23:01:03 +0200

    Add helper functions to Util.hs

---
+++ src/Game/Util.hs
@@ -0,0 +1,12 @@
+module Game.Util (generateMatrixWithCellNumbers,
+                  generateMatrixWithCellIndices) where
+
+import Data.Matrix
+import Game.Board (Dimension, coordinateToCellNumber)
+
+
+generateMatrixWithCellNumbers :: Dimension -> Matrix Int
+generateMatrixWithCellNumbers (w,h) = matrix w h (\(i,j) -> coordinateToCellNumber (i,j) (w,h))
+
+generateMatrixWithCellIndices :: Dimension -> Matrix String
+generateMatrixWithCellIndices (w,h) = matrix w h (\(i,j) -> (show i) ++ "/" ++ (show j))
