7756da344ea4f1889a57d08c5cfc7c9b3b593e00
Andy Griesbeck <andy.griesbeck@gmail.com>
2020-09-28 12:13:13 +0200

    Fix wrong comment

--- src/Marshalling.hs
+++ src/Marshalling.hs
@@ -72,8 +72,8 @@ cellToCellEntity cell _gameStatus =
       _cellEntityCoordY = snd $ cell ^. coordinate,
       _cellEntityIsFlagged = cell ^. isFlagged,
       _cellEntityIsRevealed = cell ^. isRevealed,
-      _cellEntityHasBomb = isGameOverStatus _gameStatus && (cell ^. hasBomb), -- if game is over set hasBomb to False
-      _cellEntityNeighboringBombs = if isGameOverStatus _gameStatus then cell ^. neighboringBombs else -1, -- if game is over set neighboringBombs to -1
+      _cellEntityHasBomb = isGameOverStatus _gameStatus && (cell ^. hasBomb), -- if game is not over yet, set hasBomb to False
+      _cellEntityNeighboringBombs = if isGameOverStatus _gameStatus then cell ^. neighboringBombs else -1, -- if game is not over yet, set neighboringBombs to -1
       _cellEntityAssetId = getCellAssetId _gameStatus cell
     }
 
